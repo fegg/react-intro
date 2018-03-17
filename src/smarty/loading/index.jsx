0552e1f5c957c9bbdcd2b7431933611136561088
lulin <lulin@gridsum.com>
2018-03-17 23:22:13 +0800

    test

--- src/smarty/loading/index.jsx
+++ src/smarty/loading/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './loading.css';
 
 const Loading = props => {
-    return <img className="st-loading-icon" width="32" height="32"
+    return <img className="st-loading-icon"
+        width="32" height="32"
         alt="loading"
         src={require('../../assets/loading.gif')} />;
 };
